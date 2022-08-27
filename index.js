00ba87f406ce803a27f139ab511012373154e940
rezwanrahimr <rezwanrahim69@gmail.com>
2022-08-27 17:25:45 +0600

    db connected

--- index.js
+++ index.js
@@ -2,30 +2,44 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
 
 
-// Midel ware
+//midelware
 app.use(cors());
 app.use(express.json());
 
-// MondoDB
+
+// MongoDB Connected.
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.m4rxkkc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m4rxkkc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db')
-  // perform actions on the collection object
-  client.close();
-});
 
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
+async function run() {
+    try {
+        await client.connect();
+        const ProductCollectin = client.db('productCollection').collection('product');
+
+        app.get('/products', async (req, res) => {
+            const quary = {};
+            const cursor = ProductCollectin.find(quary);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('hello world')
 })
-app.listen(port,()=>{
-    console.log(`Server is Running ${port}`);
-})
 
+app.listen(port, () => {
+    console.log(`server is running ${port}`)
+})
